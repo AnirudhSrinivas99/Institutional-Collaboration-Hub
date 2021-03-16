45d7abe1b94e825c87794bdd61b00b5b5b55c80c
AnirudhSrinivas99 <60007228+AnirudhSrinivas99@users.noreply.github.com>
2021-03-16 21:51:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// Make sure to copy your keys from your firebase console!
+
+export default FirebaseKeys = {
+  apiKey: "AIzaSyDTXVuH8ubqZlFfTl3pm1ayJmjvuXRtdng",
+  authDomain: "ich-1fefd.firebaseapp.com",
+  projectId: "ich-1fefd",
+  storageBucket: "ich-1fefd.appspot.com",
+  messagingSenderId: "703274074327",
+  appId: "1:703274074327:web:ab62c2be0c3e1b8f89f092",
+  measurementId: "G-BYDW80Z265"
+};
+
---
+++ slides.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: '1',
+        title: 'Get Recognized',
+        description: 'Get recognized in your peer group and beyond.',
+        image: require('./assets/images/creditcard.png'),
+    },
+    {
+        id: '2',
+        title: 'Collaboration',
+        description: 'Make team engagement and collaboration more faster and hasselfree',
+        image: require('./assets/images/computer.png'),
+    },
+    {
+        id: '3',
+        title: 'Instant Notifications',
+        description: 'Instant notifications keeps you updated in our app.',
+        image: require('./assets/images/notifications.png'),
+    },
+    {
+        id: '4',
+        title: 'Posts',
+        description: 'Your uploads might take around 1 hour to be reflected in our feed, since we verify every data we collect.',
+        image: require('./assets/images/customize.png'),
+    },
+];
